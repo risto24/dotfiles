1c42ae413b0a2f1bf0e1098b0ec795e311eb65a1
risto24 <46432541+risto24@users.noreply.github.com>
2023-01-22 18:38:19 +0900

    comment out notion

--- brew.sh
+++ brew.sh
@@ -26,7 +26,7 @@ brew install --cask sequel-ace
 brew install --cask google-japanese-ime
 brew install --cask keyboardcleantool
 brew install --cask rectangle
-brew install --cask notion
+# brew install --cask notion
 
 brew install vim
 brew install bat
